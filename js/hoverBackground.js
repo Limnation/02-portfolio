3b3610ce6a2a741fc3d97b7eedfc61807f94d0f4
Scott Erwin <scottallenerwin@gmail.com>
2021-02-27 16:04:54 -0500

    added in onclick and hover states to Header and portfolio sections with additions css tweaks

---
+++ js/hoverBackground.js
@@ -0,0 +1,98 @@
+// navBar background image changing
+function onHover() {
+  document.getElementById("intro").style.backgroundImage = "url('../images/header/heroImage2.png')";
+
+  document.getElementById("heroText").style.display = "none";
+}
+
+function offHover() {
+  document.getElementById("intro").style.backgroundImage = "url('../images/header/heroImage.png')";
+
+  document.getElementById("heroText").style.display = "inline-block";
+}
+
+// portfolio companies image changin
+// 1
+function onHoverPtItem1On() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/amgDevices.png')";
+}
+
+function onHoverPtItem1Off() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/devices.png')";
+}
+
+function onclickPtItem1() {
+  window.open("https://www.mercedes-amg.com/en/home.html");
+}
+// 2
+function onHoverPtItem2On() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/cigDevices.png')";
+}
+
+function onHoverPtItem2Off() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/devices.png')";
+}
+
+function onclickPtItem2() {
+  window.open("https://www.cigaretteracing.com/");
+}
+// 3
+function onHoverPtItem3On() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/ehrDevices.png')";
+}
+
+function onHoverPtItem3Off() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/devices.png')";
+}
+
+function onclickPtItem3() {
+  window.open("https://www.ehrmann.com/home");
+}
+// 4
+function onHoverPtItem4On() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/agvDevices.png')";
+}
+
+function onHoverPtItem4Off() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/devices.png')";
+}
+
+function onclickPtItem4() {
+  window.open("https://www.agv.com/us/en/");
+}
+// 5
+function onHoverPtItem5On() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/omlDevices.png')";
+}
+
+function onHoverPtItem5Off() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/devices.png')";
+}
+
+function onclickPtItem5() {
+  window.open("https://www.onemagnolialane.com/");
+}
+// 6
+function onHoverPtItem6On() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/eciDevices.png')";
+}
+
+function onHoverPtItem6Off() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/devices.png')";
+}
+
+function onclickPtItem6() {
+  window.open("https://www.electricalcontactsint.com/");
+}
+// 7
+function onHoverPtItem7On() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/sdcDevices.png')";
+}
+
+function onHoverPtItem7Off() {
+  document.getElementById("devices").style.backgroundImage = "url('../images/protfolio/devices.png')";
+}
+
+function onclickPtItem7() {
+  window.open("http://www.salonedicapelliptc.com/");
+}
